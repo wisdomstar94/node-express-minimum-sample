369a9f391f0547a10e050218718894f193d1cf42
wisdomstar94 <wisdomstar94@gmail.com>
2022-04-26 23:12:34 +0900

    multer 값 확인하는 코드 추가

--- src/routes/api/childRoutes/file.js
+++ src/routes/api/childRoutes/file.js
@@ -48,6 +48,7 @@ const uploadFileUpload = multer({
 
 router.post('/upload', uploadFileUpload.single('upfile'), function(req, res, next) {
   console.log('req.file', req.file);
+  console.log('req.body', req.body);
 
   res.status(200).json({
     result: 'success',
